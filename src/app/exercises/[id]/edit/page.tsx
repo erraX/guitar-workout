598f57cefcb27d70af75b75f3e06353847d8dde7
xx <xx@outlook.com>
2024-12-07 17:28:35 +0800

    codemode next-async-request-api

--- src/app/exercises/[id]/edit/page.tsx
+++ src/app/exercises/[id]/edit/page.tsx
@@ -2,11 +2,12 @@ import { getExerciseById } from "@/service/exercise";
 import ExerciseFormContainer from "./ExerciseFormContainer";
 import { notFound } from "next/navigation";
 
-export default async function ExercisesEditPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function ExercisesEditPage(
+  props: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
   const exercise = await getExerciseById(Number(params.id));
   if (!exercise) {
     return notFound();
--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -2,11 +2,12 @@ import { getExercises } from "@/service/exercise";
 import { getWorkoutById } from "@/service/workout";
 import { Workouts } from "./_components/Workouts";
 
-export default async function WorkoutsPage({
-  searchParams,
-}: {
-  searchParams: { workoutId: string };
-}) {
+export default async function WorkoutsPage(
+  props: {
+    searchParams: Promise<{ workoutId: string }>;
+  }
+) {
+  const searchParams = await props.searchParams;
   const workoutId = Number(searchParams.workoutId);
 
   let workout: Awaited<ReturnType<typeof getWorkoutById>> | null = null;
