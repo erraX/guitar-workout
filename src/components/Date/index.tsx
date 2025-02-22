a321f8d2583812f87ec2d817354fc2fee4cf68de
xx <xx@outlook.com>
2025-02-22 19:38:26 +0800

    migrate Sekeleton to shadcn

--- src/components/Date/index.tsx
+++ src/components/Date/index.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-import { Skeleton } from "@nextui-org/react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const DateNoSSR = dynamic(() => import("./Date"), {
   ssr: false,
